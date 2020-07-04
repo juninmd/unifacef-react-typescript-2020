40f1348f4ec1ab84d15de5ba91726bdd1f951b08
Antonio <jr_acn@yahoo.com.br>
2020-07-04 12:13:28 -0300

    teste one signal

--- src/index.tsx
+++ src/index.tsx
@@ -34,4 +34,4 @@ ReactDOM.render(
   </React.StrictMode>,
   rootElement
 );
-serviceWorker.unregister();
+serviceWorker.register();
--- src/plugins/one-signal.plugin.ts
+++ src/plugins/one-signal.plugin.ts
@@ -1,11 +1,43 @@
-import OneSignal from 'react-onesignal';
+// import OneSignal from 'react-onesignal';
 import { configs } from '../configs';
 
-const options = { autoRegister: true, autoResubscribe: true, notifyButton: { enable: true } }
+// const options = { autoRegister: true, autoResubscribe: true, notifyButton: { enable: true } }
 
-OneSignal.initialize(configs.onesignal, options);
+// OneSignal.initialize(configs.onesignal, options);
 
-try {
-    OneSignal.registerForPushNotifications();
-} catch (error) {
-}
+// try {
+//   OneSignal.registerForPushNotifications();
+// } catch (error) {
+// }
+
+
+const script = document.createElement("script") as any;
+script.id = 'signalScript';
+script.src = "https://cdn.onesignal.com/sdks/OneSignalSDK.js";
+script.async = true;
+script.onreadystatechange = async () => {
+  var OneSignal = (window as any).OneSignal || [];
+  OneSignal.push(() => {
+    const registering = OneSignal.registerForPushNotifications({
+      modalPrompt: true
+    }).then(() => {
+      console.log("in registration")
+      OneSignal.getUserId().then(userId => {
+        console.log("One signal -> ", userId)
+      }).catch((err) => {
+        console.log("error don show", err);
+      })
+    });
+    console.log("coming second", registering)
+  });
+};
+script.onload = script.onreadystatechange as any;
+script.innerHTML = `var OneSignal = window.OneSignal || [];
+                   OneSignal.push(function () {
+                     OneSignal.init({
+                       appId: "${configs.onesignal}"
+                     });
+                   });`;
+document.body.appendChild(script);
+const signalScript = document.getElementById('signalScript')!.innerHTML;
+window.eval(signalScript);
