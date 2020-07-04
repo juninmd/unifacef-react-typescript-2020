bd3a0f666e2ebfccdba19ead807ed2916d7dd998
Antonio <jr_acn@yahoo.com.br>
2020-07-04 13:00:59 -0300

    onesignal

--- src/plugins/one-signal.plugin.ts
+++ src/plugins/one-signal.plugin.ts
@@ -1,7 +1,13 @@
 import OneSignal from 'react-onesignal';
 import { configs } from '../configs';
+import { OneSignalOptions } from 'react-onesignal/dist/oneSignal.types';
 
-const options = { autoRegister: true, autoResubscribe: true, notifyButton: { enable: true } }
+const options: OneSignalOptions = {
+  autoRegister: true,
+  autoResubscribe: true,
+  notifyButton: { enable: true, showCredit: true },
+  persistNotification: true
+}
 
 OneSignal.initialize(configs.onesignal, options);
 
