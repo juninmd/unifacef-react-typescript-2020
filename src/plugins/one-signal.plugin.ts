cf701c5ea94a0101c052d93d466acdd2250c4fcb
Antonio <jr_acn@yahoo.com.br>
2020-07-04 12:52:06 -0300

    one signal

--- src/plugins/one-signal.plugin.ts
+++ src/plugins/one-signal.plugin.ts
@@ -1,43 +1,15 @@
-// import OneSignal from 'react-onesignal';
+import OneSignal from 'react-onesignal';
 import { configs } from '../configs';
 
-// const options = { autoRegister: true, autoResubscribe: true, notifyButton: { enable: true } }
+const options = { autoRegister: true, autoResubscribe: true, notifyButton: { enable: true } }
 
-// OneSignal.initialize(configs.onesignal, options);
+OneSignal.initialize(configs.onesignal, options);
 
-// try {
-//   OneSignal.registerForPushNotifications();
-// } catch (error) {
-// }
+async function init() {
+  try {
+    await OneSignal.registerForPushNotifications();
+  } catch (error) {
+  }
+}
 
-const script = document.createElement("script") as any;
-script.id = 'signalScript';
-script.src = "https://cdn.onesignal.com/sdks/OneSignalSDK.js";
-script.async = true;
-script.onreadystatechange = async () => {
-  var OneSignal = (window as any).OneSignal || [];
-  OneSignal.push(() => {
-    const registering = OneSignal.registerForPushNotifications({
-      modalPrompt: true
-    }).then(() => {
-      console.log("in registration")
-      OneSignal.getUserId().then(userId => {
-        console.log("One signal -> ", userId)
-      }).catch((err) => {
-        console.log("error don show", err);
-      })
-    });
-    console.log("coming second", registering)
-  });
-};
-script.onload = script.onreadystatechange as any;
-script.innerHTML = `var OneSignal = window.OneSignal || [];
-                   OneSignal.push(function () {
-                     OneSignal.init({
-                       appId: "${configs.onesignal}"
-                     });
-                   });`;
-document.body.appendChild(script);
-const signalScript = document.getElementById('signalScript')!.innerHTML;
-/* eslint no-eval: 0 */
-window.eval(signalScript);
+init();
